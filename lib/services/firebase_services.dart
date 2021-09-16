399749adc2c019423a11ff2b6637af0eb8f88015
keungsong <80748921+keungsong@users.noreply.github.com>
2021-09-16 21:41:07 +0700

    upload data

--- lib/services/firebase_services.dart
+++ lib/services/firebase_services.dart
@@ -5,6 +5,24 @@ class FirebaseServices {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   CollectionReference banners =
       FirebaseFirestore.instance.collection('ImageSlide');
+      CollectionReference productData = 
+      FirebaseFirestore.instance.collection('ProductData');
 
   FirebaseStorage storage = FirebaseStorage.instance;
+
+
+
+  Future <String>uploadDataToDb(url,name,description)async{
+    String downloadUrl = await storage.ref(url).getDownloadURL();
+    if(downloadUrl != null){
+     productData.doc(name).set({
+        'image':downloadUrl,
+        'name':name,
+        'description':description,
+
+      });
+    }
+    return downloadUrl;
+  }
+
 }
